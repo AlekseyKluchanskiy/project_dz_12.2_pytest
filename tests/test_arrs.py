89b7354731f03f19f45930dfa118e231203e0839
aleksey_kluchanskiy <alesha-mail@yandex.ru>
2024-04-22 14:01:51 +0300

    add fixture

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,13 +1,25 @@
+import pytest
+
 from utils import arrs
+@pytest.fixture
+def array_fixture():
+    return [1, 2, 3, 4]
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
 
+def test_get_fixture(array_fixture):
+    assert arrs.get(array_fixture, 1, "test") == 2
+    assert arrs.get(array_fixture, 3, "test") == 4
+
+
 def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([], None, None) == []
-    assert arrs.my_slice([1, 2, 3, 4], None, 3) == [1, 2, 3]
+      assert arrs.my_slice([], None, None) == []
+
+
+def test_slice_fixture(array_fixture):
+    assert arrs.my_slice(array_fixture, 1, 2) == [2]
+    assert arrs.my_slice(array_fixture, 1) == [2, 3, 4]
+    assert arrs.my_slice(array_fixture, None, 4) == [1, 2, 3, 4]
